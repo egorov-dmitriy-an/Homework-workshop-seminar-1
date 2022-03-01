35799925166e12b9c7b52dee289e45adb59e7ad0
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-03-02 00:55:12 +0300

    Add determining the maximum

---
+++ Task002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее, например:
+// a =  5 b = 7  -> max = 7 
+// a =  2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            if(number1 >= number2) Console.WriteLine("max = " + number1);
+            else Console.WriteLine("max = " + number2);
+        }
+    }
+}
