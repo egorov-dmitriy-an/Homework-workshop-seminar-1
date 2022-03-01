47496d2000781d3874f1e8011215240401e29c65
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-03-02 01:56:22 +0300

    Add Even or odd

---
+++ Task006/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 6: Напишите программу, которая на вход 
+// принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка), например:
+//  4 -> да
+// -3 -> нет 
+//  7 -> нет 
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int ostatok = number%2;
+        if(ostatok == 0) Console.WriteLine("Число " + number + " - четное");
+        else Console.WriteLine("Число " + number + " - нечетное");
+        }
+    }
+}
