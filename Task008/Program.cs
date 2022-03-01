400d27d38f278e50a3c03bc8936641e5cc2d0c8d
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-03-02 02:41:32 +0300

    Add a number of even

---
+++ Task008/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 8: Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает 
+// все чётные числа от 1 до N, например:
+//  5 -> 2, 4
+//  8 -> 2, 4, 6, 8 
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.Write("Введите число: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int n;
+        if(N >= 2) 
+        {
+            n = 2;
+            while (n <= N)
+            {
+                Console.WriteLine(n + ", ");
+                n = n + 2;
+            }
+        }
+            else if (N <= 0)
+            {
+                n = 0;
+                while (n >= N)
+                {
+                    Console.WriteLine(n + ", ");
+                    n = n - 2;
+                } 
+            }
+                else  Console.WriteLine("Чисел нет");
+        }
+    }
+}
